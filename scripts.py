0dd70b3ce0619df708e826846d8d230d65748150
Himas1321 <nikita5045@mail.ru>
2025-11-30 20:50:47 +0900

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,15 +1,21 @@
 from datacenter.models import Schoolkid, Lesson, Commendation, Chastisement, Mark
 import random
 
-def create_commendation(schoolkid_name, subject_title):
 
+def search_student(schoolkid_name):
     try:
-        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        return Schoolkid.objects.get(full_name__contains=schoolkid_name)
     except Schoolkid.MultipleObjectsReturned:
         print("Найдено несколько учеников с таким именем, уточните запрос.")
-        return
+        return None
     except Schoolkid.DoesNotExist:
         print("Ученик с таким именем не найден.")
+        return None
+
+
+def create_commendation(schoolkid_name, subject_title):
+    child = search_student(schoolkid_name)
+    if child is None:
         return
 
     lessons =  Lesson.objects.filter(
@@ -20,12 +26,23 @@ def create_commendation(schoolkid_name, subject_title):
 
     last_lesson = lessons.order_by("date").last()
 
+    if last_lesson is None:
+        print("Урок по этому предмету не найден.")
+        return
+
     praise = [
-    "Молодец!", "Отлично!"," Хорошо!",
-    "Гораздо лучше, чем я ожидал!", "Ты меня приятно удивил!", 
-    "Великолепно!", "Прекрасно!", "Ты меня очень обрадовал!",
-    "Именно этого я давно ждал от тебя!", "Сказано здорово – просто и ясно!", 
-    "Ты, как всегда, точен!", "Очень хороший ответ!","Талантливо!",
+    "Молодец!",
+    "Отлично!",
+    "Хорошо!",
+    "Гораздо лучше, чем я ожидал!",
+    "Ты меня приятно удивил!",
+    "Великолепно!", "Прекрасно!",
+    "Ты меня очень обрадовал!",
+    "Именно этого я давно ждал от тебя!",
+    "Сказано здорово – просто и ясно!",
+    "Ты, как всегда, точен!",
+    "Очень хороший ответ!",
+    "Талантливо!",
     "Ты сегодня прыгнул выше головы!"
     ]
 
@@ -37,33 +54,19 @@ def create_commendation(schoolkid_name, subject_title):
 
 
 def remove_chastisements(schoolkid_name):
-    
-    try:
-        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    except Schoolkid.MultipleObjectsReturned:
-        print("Найдено несколько учеников с таким именем, уточните запрос.")
-        return
-    except Schoolkid.DoesNotExist:
-        print("Ученик с таким именем не найден.")
+    child = search_student(schoolkid_name)
+    if child is None:
         return
 
-    bad_chastisements = Chastisement.objects.filter(schoolkid=child)
-    for bad_chastisement in bad_chastisements:
-            bad_chastisement.delete()
+    Chastisement.objects.filter(schoolkid=child).delete()
 
 
 def fix_marks(schoolkid_name):
-
-    try:
-        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    except Schoolkid.MultipleObjectsReturned:
-        print("Найдено несколько учеников с таким именем, уточните запрос.")
-        return
-    except Schoolkid.DoesNotExist:
-        print("Ученик с таким именем не найден.")
+    child = search_student(schoolkid_name)
+    if child is None:
         return
 
     bad_marks = Mark.objects.filter(schoolkid=child, points__in=[2,3])
     for bad_point in bad_marks:
-            bad_point.points = 5
-            bad_point.save()
+        bad_point.points = 5
+        bad_point.save()
